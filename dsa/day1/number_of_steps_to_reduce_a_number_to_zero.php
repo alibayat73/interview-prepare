3d2b4bf46eb3923ab49f0fd23f4f68b09b388a61
Ali Bayat <alibayat73@gmail.com>
2025-10-21 03:07:45 +0330

    Add solution for reducing number to zero

---
+++ dsa/day1/number_of_steps_to_reduce_a_number_to_zero.php
@@ -0,0 +1,21 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return Integer
+     */
+    function numberOfSteps($num) {
+        $steps = 0;
+        while($num > 0) {
+            if ($num % 2 == 0) {
+                $num /= 2;
+            } else {
+                $num -= 1;
+            }
+            $steps += 1;
+        }
+
+        return $steps;
+    }
+}
