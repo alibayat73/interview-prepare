51c72107fa968af9ed50390435675f1ba7fa28ed
Ali Bayat <alibayat73@gmail.com>
2025-10-21 03:02:16 +0330

    Implement binary search algorithm in PHP

---
+++ dsa/day1/binary_search.php
@@ -0,0 +1,27 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function search($nums, $target) {
+
+        $high = count($nums) - 1;
+        $low = 0;
+
+        while ($low <= $high) {
+            $mid = (int)(($high + $low) / 2);
+            if ($nums[$mid] == $target) {
+                return $mid;
+            } elseif ($nums[$mid] < $target) {
+                $low = $mid + 1;
+            } else {
+                $high = $mid - 1;
+            }
+        }
+
+        return -1;
+        
+    }
+}
